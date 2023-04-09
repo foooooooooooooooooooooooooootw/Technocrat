f9a9f94452420fcc89031a915ef9097100e8169a
FTW <foooooooooooooooooooooooooootw@gmail.com>
2023-04-10 06:14:30 +0800

    initial commit

---
+++ app/src/main/java/com/example/technocrat/ui/settings/SettingsFragment.java
@@ -0,0 +1,38 @@
+package com.example.technocrat.ui.settings;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.fragment.app.Fragment;
+import androidx.lifecycle.ViewModelProvider;
+
+import com.example.technocrat.databinding.FragmentNotificationsBinding;
+import com.example.technocrat.ui.settings.SettingsViewModel;
+
+public class SettingsFragment extends Fragment {
+
+    private FragmentNotificationsBinding binding;
+
+    public View onCreateView(@NonNull LayoutInflater inflater,
+                             ViewGroup container, Bundle savedInstanceState) {
+        SettingsViewModel notificationsViewModel =
+                new ViewModelProvider(this).get(SettingsViewModel.class);
+
+        binding = FragmentNotificationsBinding.inflate(inflater, container, false);
+        View root = binding.getRoot();
+
+        final TextView textView = binding.textNotifications;
+        notificationsViewModel.getText().observe(getViewLifecycleOwner(), textView::setText);
+        return root;
+    }
+
+    @Override
+    public void onDestroyView() {
+        super.onDestroyView();
+        binding = null;
+    }
+}
