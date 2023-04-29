09a98c94262930c4696067531c0b0820020a39c3
FTW <foooooooooooooooooooooooooootw@gmail.com>
2023-04-29 10:46:23 +0800

    modify activity_spoof_filenames.xml, SpoofFilenames.java, strings.xml

--- app/src/main/java/com/example/technocrat/ui/dashboard/SpoofFilenames.java
+++ app/src/main/java/com/example/technocrat/ui/dashboard/SpoofFilenames.java
@@ -4,14 +4,19 @@ import androidx.appcompat.app.ActionBar;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.constraintlayout.widget.ConstraintLayout;
 
+import android.app.Activity;
 import android.content.Context;
 import android.content.SharedPreferences;
 import android.graphics.drawable.ColorDrawable;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.Spinner;
+import android.widget.TextView;
 
 import com.example.technocrat.R;
 
-public class SpoofFilenames extends AppCompatActivity {
+public class SpoofFilenames extends AppCompatActivity implements AdapterView.OnItemSelectedListener {
 
     int theme = 0;
     int buttonTheme = 0;
@@ -19,6 +24,9 @@ public class SpoofFilenames extends AppCompatActivity {
     ConstraintLayout constraintLayout;
     ActionBar actionBar;
 
+    Spinner spinner;
+
+    TextView textView;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -26,6 +34,9 @@ public class SpoofFilenames extends AppCompatActivity {
 
         constraintLayout = findViewById(R.id.cl);
         actionBar = getSupportActionBar();
+        textView = findViewById(R.id.textView);
+        spinner = findViewById(R.id.spinner);
+        spinner.setOnItemSelectedListener(this);
         SharedPreferences settings = getSharedPreferences("theme", Context.MODE_PRIVATE);
         theme = settings.getInt("colour", 0);
         buttonTheme = settings.getInt("buttonColour", 0);
@@ -66,6 +77,25 @@ public class SpoofFilenames extends AppCompatActivity {
                     = new ColorDrawable(getResources().getColor(R.color.rose_2));
             actionBar.setBackgroundDrawable(colorDrawable);
         }
+
+    }
+
+    @Override
+    public void onItemSelected(AdapterView<?> adapterView, View view, int i, long l) {
+        String selected = spinner.getSelectedItem().toString();
+        if(selected.equals("Fake iPhone Camera Filename")){
+            textView.setText("Example: IMG_0001");
+        } else if(selected.equals("Fake Android Camera Filename")){
+            textView.setText("IMG_20230302_114348");
+        } else if(selected.equals("Fake UUID v1")){
+            textView.setText("37e9316e-e637-11ed-a05b-0242ac120003");
+        } else if(selected.equals("Fake UUID v4")){
+            textView.setText("c453eab1-ad34-4e8e-8590-8a63ed030f8a");
+        }
+    }
+
+    @Override
+    public void onNothingSelected(AdapterView<?> adapterView) {
 
     }
 }
